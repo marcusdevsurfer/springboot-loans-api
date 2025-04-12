38a88a812861080649f96143cbdbcc38db32fd7f
marcusdevsurfer <marcus.sea13@gmail.com>
2025-04-11 23:36:24 -0600

    add modelmapper dependency to pom.xml

---
+++ src/main/java/com/markdev/springboot_loans_api/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.markdev.springboot_loans_api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
