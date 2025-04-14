2790f88f0ca7789bdf415026f17e0be814b1cef8
marcusdevsurfer <marcus.sea13@gmail.com>
2025-04-13 22:41:08 -0600

    feat: rename IlegalArgumentException to IllegalArgumentException and add HTTP status annotations

--- src/main/java/com/markdev/springboot_loans_api/exceptions/IlegalArgumentException.java
+++
@@ -1,7 +0,0 @@
-package com.markdev.springboot_loans_api.exceptions;
-
-public class IlegalArgumentException extends RuntimeException {
-    public IlegalArgumentException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/markdev/springboot_loans_api/exceptions/IllegalArgumentException.java
+++ src/main/java/com/markdev/springboot_loans_api/exceptions/IllegalArgumentException.java
@@ -1,13 +1,11 @@
 package com.markdev.springboot_loans_api.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
         super(message);
     }
-
 }
